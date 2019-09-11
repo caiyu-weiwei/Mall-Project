9ecdeb3f8fc8ce25f8612c670cd64e6793b9986c
weiwei.cai <weiwei.cai@zhangmen.com>
2019-09-11 11:44:45 +0800

    编写SearchBar组件

---
+++ mall-project/src/components/SearchBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="search-bar">
+    <van-row>
+      <van-col span="3">
+        <img class="search-icon" :src="searchIcon">
+      </van-col>
+      <van-col span="16">
+        <input type="text" class="search-input">
+      </van-col>
+      <van-col span="5">
+        <van-button class="search-button">查找</van-button>
+      </van-col>
+    </van-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        searchIcon: require('../assets/images/search.png')
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+.search-bar{
+  height: 2.2rem;
+  line-height: 2.2rem;
+  background-color: #e5017d;
+}
+.search-icon{
+  width: 50%;
+  margin: .6rem 0 0 .5rem;
+}
+.search-input{
+  height: 1.3rem;
+  border: 0;
+  background-color: #e5017d;
+  color: #fff;
+  border-bottom: 1px solid #ccc;
+}
+.search-button{
+  height: 1.3rem;
+  line-height: 1.3rem;
+  background-color: #CDCACC;
+  border-radius: .3rem;
+  border: 1px solid #CDCACC;
+}
+</style>
---
+++ mall-project/src/components/ShoppingMall.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <search-bar></search-bar>
+  </div>
+</template>
+
+<script>
+  import searchBar from './SearchBar'
+  export default {
+    name: 'ShoppingMall',
+    data() {
+      return {
+        message: 'vant'
+      }
+    },
+    components: {
+      searchBar
+    },
+    methods: {
+      getSourceData() {}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
