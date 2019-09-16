173dc73333432b46130dc5c2f6869cc387802ee9
weiwei.cai <weiwei.cai@zhangmen.com>
2019-09-16 16:47:15 +0800

    对数据库连接异常处理包含最大连接次数等等

--- mall-project/service/database/init.js
+++ mall-project/service/database/init.js
@@ -7,20 +7,38 @@ const db = 'mongodb://localhost/mall-db'
 exports.connect = () => {
   // 链接数据库
   mongoose.connect(db)
+  let maxConnectTimes = 0
 
-  // 添加数据库连接事件监听
-  mongoose.connection.on('disconnected', () => {
-    mongoose.connect(db)
-  })
+  return new Promise((resolve, reject) => {
 
-  // 数据库连接出错时
-  mongoose.connection.on('error', err => {
-    console.log('err', err)
-    mongoose.connect(db)
-  })
+    // 添加数据库连接事件监听
+    mongoose.connection.on('disconnected', () => {
+      console.log('**************数据库断开**************')
+      if (maxConnectTimes < 3) {
+        maxConnectTimes++
+        mongoose.connect(db)
+      } else {
+        reject()
+        throw new Error('数据库出现问题，程序无法搞定，请人为修复......')
+      }
+    })
 
-  mongoose.connection.once('open', () => {
-    console.log('MongoDB connected successfully')
+    // 数据库连接出错时
+    mongoose.connection.on('error', err => {
+      console.log('****************数据库错误******************')
+      if (maxConnectTimes < 3) {
+        maxConnectTimes++
+        mongoose.connect(db)
+      } else {
+        reject(err)
+      }
+    })
+
+    mongoose.connection.once('open', () => {
+      console.log('MongoDB connected successfully')
+      resolve()
+    })
+
   })
 
 }
